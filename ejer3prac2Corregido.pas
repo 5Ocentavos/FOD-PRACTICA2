623d0143f77483ef63d6c40fd9421448d5f70d47
Mariángeles Muñoz Loza <81397943+5Ocentavos@users.noreply.github.com>
2022-04-07 12:11:39 -0300

    Update and rename ejer3prac2NoCorregido.pas to ejer3prac2Corregido.pas

--- ejer3prac2Corregido.pas
+++ ejer3prac2Corregido.pas
@@ -5,16 +5,20 @@ stock mínimo y precio del producto.
 Se recibe diariamente un archivo detalle de cada una de las 30 sucursales de la cadena. Se
 debe realizar el procedimiento que recibe los 30 detalles y actualiza el stock del archivo
 maestro. La información que se recibe en los detalles es: código de producto y cantidad
-vendida. Además, se deberá informar en un archivo de texto: nombre de producto,
+vendida. 
+Además, se deberá informar en un archivo de texto: nombre de producto,
 descripción, stock disponible y precio de aquellos productos que tengan stock disponible por
 debajo del stock mínimo.
+
 Nota: todos los archivos se encuentran ordenados por código de productos. En cada detalle
 puede venir 0 o N registros de un determinado producto.
 }
 
-
 program ejer3prac2;
 uses SysUtils;  ///LPM 
+const 
+  cantSucursales = 30;
+  valorAlto = 9999;
 type
   producto = record
     codProd: integer;
@@ -34,28 +38,60 @@ type
   
   archivoVentas = file of venta;
   
-  vectorArchivoVentas = array [1..30] of archivoVentas;
-  vectorRegistroVenta = array [1..30] of venta;
+  vectorArchivoVentas = array [1..cantSucursales] of archivoVentas;
+  vectorRegistroVenta = array [1..cantSucursales] of venta;
   
+  
+procedure listarStockPorDebajo (var archMaestro: archivoProductos);
+var
+  archText: text;
+  prod: producto;
+begin
+  assign (archText, 'productos_debajo_del_stockMin');
+  rewrite (archText);
+  reset (archMaestro);
+  while (not EOF (archMaestro)) do
+    begin
+      read (archMaestro, prod);
+      if (prod.stockDisp < prod.stockMin) then
+        begin
+          writeln (archText,prod.nombre);
+          writeln (archText, prod.stockDisp);
+          writeln (archText, prod.precio);
+          writeln (archText, prod.descripcion);
+        end;
+    end;
+  close (archMaestro);
+  close (archText);
+end;  
   
 procedure cerrarArchivoDetalles (var vecArchDetalles: vectorArchivoVentas);
 var 
   i: integer;
 begin
-  for i := 1 to 30 do
+  for i := 1 to cantSucursales do
     begin
       close (vecArchDetalles [i]);
     end;
 
 end;
   
+procedure leerDetalle (var archDetalle: archivoVentas; var vent:venta);
+begin
+  if (not EOF (archDetalle)) then
+    read (archDetalle, vent)
+  else
+    vent.codProd := valorAlto;
+end;
 
-procedure buscarMinimoYActualizar (var vecArchDetalles: vectorArchivoVentas; var vecRegDetalles: vectorRegistroVenta;                                    var ventaMin: venta);
+
+procedure buscarMinimoYActualizar (var vecArchDetalles: vectorArchivoVentas; var vecRegDetalles: vectorRegistroVenta; 
+                                   var ventaMin: venta);
 var
   i, pos: integer;
 begin
-  ventaMin.codProd := 9999;
-  for i:= 1 to 30 do 
+  ventaMin.codProd := valorAlto;
+  for i:= 1 to cantSucursales do 
     begin
       if (vecRegDetalles[i].codProd < ventaMin.codProd) then
         begin
@@ -63,11 +99,8 @@ begin
           pos := i;
         end;
     end;
-  if (ventaMin.codProd <> 9999) then
-    if (not EOF (vecArchDetalles[pos])) then
-      read (vecArchDetalles[pos], vecRegDetalles[pos])
-    else
-      vecRegDetalles [pos].codProd := 9999;
+  if (ventaMin.codProd <> valorAlto) then
+     leerDetalle (vecArchDetalles [pos], vecRegDetalles [pos]);
 end;
                                    
   
@@ -75,13 +108,17 @@ procedure cargarVectorRegistros (var vecArchDetalles: vectorArchivoVentas; var v
 var
   i: integer;
 begin
-  for i := 1 to 30 do
-    begin
-      if (not EOF (vecArchDetalles [i])) then
-        read (vecArchDetalles[i], vecRegDetalles [i])
-      else
-        vecRegDetalles [i].codProd := 9999;
-    end;
+  for i := 1 to cantSucursales do
+    leerDetalle (vecArchDetalles [i], vecRegDetalles [i]);
+end;
+
+
+procedure resetVectorArchivoDetalles (var vecArchDetalle: vectorArchivoVentas);
+var 
+  i: integer;
+begin
+  for i := 1 to cantSucursales do
+    reset (vecArchDetalle [i]);
 end;
 
 
@@ -91,9 +128,11 @@ var
   ventaMin, ventaAct: venta;
   vecRegDetalles: vectorRegistroVenta;
 begin
+  reset (archMaestro);
+  resetVectorArchivoDetalles (vecArchDetalles);
   cargarVectorRegistros (vecArchDetalles, vecRegDetalles);
   buscarMinimoYActualizar (vecArchDetalles, vecRegDetalles, ventaMin);  
-  while (ventaMin.codProd <> 9999) do 
+  while (ventaMin.codProd <> valorAlto) do 
     begin
       ventaAct.codProd := ventaMin.codProd;
       ventaAct.cantVend := 0;
@@ -104,42 +143,25 @@ begin
         end;
       //busco el codigo en el maestro
       read (archMaestro, prod);
-      while ((not EOF (archMaestro)) and (prod.codProd <> ventaAct.codProd)) do
+      while (prod.codProd <> ventaAct.codProd) do
         read (archMaestro, prod);
       prod.stockDisp := prod.stockDisp - ventaAct.cantVend;
       seek (archMaestro, filePos (archMaestro) -1);
       write (archMaestro, prod);
     end;
+  close (archMaestro);
+  cerrarArchivoDetalles (vecArchDetalles);
 end;
 
-
-procedure hacerAssignDeLosDetallesYReset (var vecArchDetalle: vectorArchivoVentas);
+procedure assignVectorArchivoDetalles (var vecArchDetalles: vectorArchivoVentas);
 var 
   i: integer;
   nomArch: string;
 begin
-  for i := 1 to 30 do
+  for i := 1 to cantSucursales do
     begin
       nomArch := ('detalle_'+IntToStr(i));
-      assign (vecArchDetalle [i], nomArch);
-      reset (vecArchDetalle [i]);
+      assign (vecArchDetalles [i], nomArch);
     end;
 end;
-
-
-var
-  archMaestro : archivoProductos;
-  vecArchDetalles: vectorArchivoVentas;  
-
-
-BEGIN
-  assign (archMaestro, 'archivo_maestro');
-  reset (archMaestro);
-  hacerAssignDeLosDetallesYReset (vecArchDetalles);
-  actualizarMaestro (archMaestro, vecArchDetalles);
-  close (archMaestro);
-  cerrarArchivoDetalles (vecArchDetalles);
-//  listarStockPorDebajo (archivoMaestro);
-END.
-
 
